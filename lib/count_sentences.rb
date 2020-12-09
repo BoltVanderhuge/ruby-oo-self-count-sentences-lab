c15c27674c6645608cabd4a180ea2eeeec8598f7
john_wisneski <john.j.wisneski@gmail.com>
2020-12-08 21:04:46 -0500

    commit tree 6fc342acdd84c2e7e875e47cf8edfb0de02e9bdb

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
+  # def count_sentences
+  #   self.split(/[!?.]/).reject(&:empty?).length
+  # end
   def count_sentences
-
+    total_sentences = self.split(/[!?.]/)
+    total_sentences.select {|sentence| sentence != ""}.length
   end
+
+  #  binding.pry
+
 end
